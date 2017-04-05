0b2e5bcecf1bd6bb0bc0f998ecaf180881ccb454
Alberto Mur <565825@unizar.es>
2017-04-05 16:30:28 +0200

    UDPFTPServer refactor

--- src/ftp/server/UDPFTPServer.java
+++ src/ftp/server/UDPFTPServer.java
@@ -2,107 +2,138 @@ package ftp.server;
 
 import ftp.FTPService;
 
-import java.io.File;
+import java.io.IOException;
 import java.net.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by StFrancisco on 21/03/2017.
  */
 public class UDPFTPServer {
 
-    static private String ftpPath = "ftp/";
+    static final String FTP_ROOT = "ftp/";
+
+    private ExecutorService executor;
+
+    private DatagramSocket s;
+    private DatagramPacket p;
 
     public static void main(String[] args) {
 
-        byte[] buf = new byte[FTPService.SIZEMAX];
-        String sRX;
-        String sTX;
+        if (!Files.isReadable(Paths.get(FTP_ROOT))) {
+            System.out.println("FTP Root is not readable");
+            System.exit(1);
+        }
 
-        int rPort;
-        InetAddress rHost;
-        //InetAddress lHost;
-        String lHostString = "localhost";// Cambiar esto en version final
-        int lPort = FTPService.SERVERPORT;
-        int lPortTCP;
+        switch (args.length) {
+            case 0:
+                new UDPFTPServer(FTPService.SERVERPORT);
+                break;
+            case 1:
+                new UDPFTPServer(Integer.parseInt(args[0]));
+                break;
+        }
+    }
 
-        DatagramSocket s;
-        DatagramPacket packet;
+    public UDPFTPServer(int port) {
+
+        executor = Executors.newFixedThreadPool(20);
 
         try{
 
-            //lHost  = InetAddress.getByName(lHostString);
-            s = new DatagramSocket(FTPService.SERVERPORT); //Este escucha
-            packet = new DatagramPacket(buf, buf.length);
-            System.out.println("Server is listening on " + lPort);
+            s = new DatagramSocket(port); //Este escucha
+            p = new DatagramPacket(new byte[FTPService.SIZEMAX], FTPService.SIZEMAX);
+
+            System.out.println("Server is listening on " + port);
 
             while (true) {
 
-                s.receive(packet);
-                rPort = packet.getPort();
-                rHost = packet.getAddress(); //adquirimos direcciony puerto de peticion
-                sRX = new String(packet.getData(),0, packet.getLength());
-                System.out.println("Received " + rHost.getHostAddress() +
-                                ":" + rPort + " " + sRX);
+                s.receive(p);
 
-                sTX = parseCommand(sRX, rPort);
+                System.out.println("Received " + p.getAddress().getHostAddress()
+                        + ":" + p.getPort() + " "
+                        + new String(p.getData(),0, p.getLength()));
 
-                FTPService.sendUDPmessage(s, sTX, rHost, rPort);
-                System.out.println("Responded with: " + sTX);
+                this.handleCommand();
 
-                packet.setLength(buf.length);
+                p.setLength(FTPService.SIZEMAX);
             }
 
-        } catch (Exception bx) {
-            System.out.println("Exception caught while running ftp.server: \n"
-                + bx + "\nexiting");
+        }catch (SocketException e) {
+            System.out.println("Unable to bind port " + port);
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println(e.getStackTrace().toString());
             System.exit(1);
         }
     }
 
-    static String parseCommand(String sRX, int rPort) {
+    private void helloAction() throws IOException{
+        String rx = FTPService.Response.WCOME.toString();
 
-        FTPService.Command cRX = FTPService.commandFromString(sRX);
-        String sTX;
+        rx = rx + " Welcome to DUFTP Server";
+        FTPService.sendUDPmessage(this.s, rx, this.p.getSocketAddress());
+    }
 
-        switch(cRX){
-            case HELLO:
-                sTX =  FTPService.Response.WCOME.toString();
-                break;
+    private void quitAction() throws IOException{
+        String rx = FTPService.Response.BYE.toString();
+        FTPService.sendUDPmessage(this.s, rx, this.p.getSocketAddress());
+    }
 
-            case LIST:
-                HandleFTPConnection tcpList = new HandleFTPConnection(0, rPort, ftpPath); //OS elige puerto
-                int lPortTCPList = tcpList.getlPort(); //obetemos puerto asignado
-                new Thread(tcpList).start(); //lanzamos hilo
-                sTX =  (FTPService.Response.PORT.toString() + " " + lPortTCPList); //avisamos cliente
-                break;
+    private void listAction(String request) {
+        // TODO HandleList call
+        System.out.println("Handle FTP Connection for " + this.p.getAddress().getHostAddress());
+    }
 
-            case GET:
-                String fName = ftpPath + FTPService.requestedFile(sRX); //obtenemos nombre fichero
-                File f = new File(fName);//obtenemos fichero
-
-                if(f.exists() && !f.isDirectory()) { //miramos si existe
-
-                    HandleFTPConnection tcpGet = new HandleFTPConnection(0, rPort, f);//OS elige puerto
-                    int lPortTCPGet = tcpGet.getlPort(); //obtenemos puerto
-                    new Thread(tcpGet).start(); //lanzamos hilo
-                    sTX = (FTPService.Response.PORT.toString() + " " + lPortTCPGet); //avisamos cliente
-
-                } else { //Si no existe devolvemos error
-                    System.out.println(fName + ": File does not exist or is a directory");
-                    sTX = FTPService.Response.SERVERROR.toString(); // si no existe devolvemos error**?
-                }
-               break;
-
-            case QUIT:
-                sTX =  FTPService.Response.BYE.toString();
-                break;
-
-            default:
-                sTX =  FTPService.Response.UNKNOWN.toString(); //Responder con esto o con SERVERROR?
-                break;
+    private void getAction(String request) throws IOException {
+        String rx;;
+        String file;
+        int[] chunkIntval;
+        try {
+            file = FTPService.requestedFile(request);
+        } catch (Exception e) {
+            rx = FTPService.Response.SERVERROR.toString();
+            rx += " " + e.getMessage();
+            FTPService.sendUDPmessage(this.s, rx, this.p.getSocketAddress());
+            return;
         }
 
-        return  sTX;
+        // TODO implement GetHandle
+    }
+
+    private void handleCommand(){
+        String sRX = new String(p.getData(),0, p.getLength());
+        FTPService.Command cRX = FTPService.commandFromString(sRX);
+        try {
+            switch (cRX) {
+                case HELLO:
+                    this.helloAction();
+                    break;
+
+                case LIST:
+                    this.listAction(sRX);
+                    break;
+
+                case GET:
+                    this.getAction(sRX);
+                    break;
+
+                case QUIT:
+                    this.quitAction();
+                    break;
+
+                default:
+                    FTPService.sendUDPmessage(this.s,
+                            FTPService.Response.UNKNOWN.toString(),
+                            this.p.getSocketAddress());
+                    break;
+            }
+        } catch (IOException e) {
+            System.out.println(e.getStackTrace().toString());
+        }
     }
 
 }
