34e93e8f17db4d24866edce632606246b1b12b55
Alberto Mur <565825@unizar.es>
2017-04-19 11:25:56 +0200

    Added name property to RemoteServer

--- src/ftp/client/Session/RemoteServer.java
+++ src/ftp/client/Session/RemoteServer.java
@@ -6,14 +6,28 @@ import java.net.InetAddress;
  * Created by StFrancisco on 10/04/2017.
  */
 public class RemoteServer {
+
+    private String name;
     private int bw;
     private InetAddress addr;
     private int port;
 
-    public RemoteServer(InetAddress addr, int port, int bw) {
+    private int dataPort;
+
+    public RemoteServer(String name, InetAddress addr, int port, int bw) {
+        this.name = name;
         this.bw = bw;
         this.addr = addr;
         this.port = port;
+    }
+
+    public RemoteServer(InetAddress addr, int port, int bw) {
+        this(addr.getHostName(), addr, port, bw);
+        this.name = addr.getHostName() + ":" + port;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getBw() {
@@ -26,5 +40,9 @@ public class RemoteServer {
 
     public int getPort() {
         return port;
+    }
+
+    public String toString(){
+        return this.name;
     }
 }
